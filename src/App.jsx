1b5d2f6d4326fe93b92925eb830f6aeeb6e9e770
keshav <keshavswaraj75@gmail.com>
2026-02-15 12:54:02 +0530

    feat: Initialize project with Vite, React, Tailwind CSS, and ESLint, along with adding initial media assets and basic styling.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-6xl text-orange-500 font-circular-web'>Welcome to Awwards</h1>
+    </main>
+  )
+}
+
+export default App
