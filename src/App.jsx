709763c3e0ab7d9ac91a9dd1ff92331e3ee54d7e
keshav <keshavswaraj75@gmail.com>
2026-02-22 13:19:28 +0530

    feat: Implement initial application structure with core components, global styling, and GSAP animations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-6xl text-orange-500 font-circular-web'>Welcome to Awwards</h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
+      <Navbar />
+      <Hero />
+      <About />
     </main>
   )
 }
